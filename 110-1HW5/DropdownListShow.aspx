c216ed5f4c0b86e2544845c81fe95c1684417c61
翔澤 蕭 <108111137@mail.aeust.edu.tw>
2021-11-19 12:20:42 +0800

    20211119

--- 110-1HW5/DropdownListShow.aspx
+++ 110-1HW5/DropdownListShow.aspx
@@ -10,6 +10,18 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <asp:DropDownList ID="ddl_Area" runat="server" OnSelectedIndexChanged="ddl_Area_SelectIndexChanged" AutoPostBack="True">
+                <asp:ListItem>北區</asp:ListItem>
+                <asp:ListItem>中區</asp:ListItem>
+            </asp:DropDownList><br />
+
+            <asp:DropDownList ID="dll_Place" runat="server">
+                <asp:ListItem>基隆</asp:ListItem>
+                <asp:ListItem>台北</asp:ListItem>
+                <asp:ListItem>新北</asp:ListItem>
+                <asp:ListItem>苗栗</asp:ListItem>
+                <asp:ListItem>台中</asp:ListItem>
+            </asp:DropDownList>
         </div>
     </form>
 </body>
--- 110-1HW5/DropdownListShow.aspx.cs
+++ 110-1HW5/DropdownListShow.aspx.cs
@@ -8,7 +8,35 @@ using System.Web.UI.WebControls;
 namespace _110_1HW5 {
     public partial class DropdownListShow : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
+           /*ListItem o_Li = new ListItem("北區", "北區");
+            ddl_Area.Item.add(o_Li);
 
+            o_Li = new ListItem("中區", "中區");
+            ddl_Area.Item.add(o_Li);
+
+            o_Li = new ListItem("基隆", "基隆");
+            ddl_Area.Item.add(o_Li);
+            o_Li = new ListItem("台北", "台北");
+            ddl_Area.Item.add(o_Li);
+            o_Li = new ListItem("新北", "新北");
+            ddl_Area.Item.add(o_Li);*/
+        }
+        protected void ddl_Area_SelectIndexChanged(object sender, EventArgs e)
+        {
+            /*if (ddl_Area_SelectedValue == "北區") {
+                o_Li = new ListItem("基隆", "基隆");
+            ddl_Area.Item.add(o_Li);
+                o_Li = new ListItem("台北", "台北");
+            ddl_Area.Item.add(o_Li);
+                o_Li = new ListItem("新北", "新北");
+            ddl_Area.Item.add(o_Li); 
+            }
+            else{
+                o_Li = new ListItem("苗栗", "苗栗");
+            ddl_Area.Item.add(o_Li);
+                o_Li = new ListItem("台中", "台中");
+            ddl_Area.Item.add(o_Li); 
+            }*/
         }
     }
 }
